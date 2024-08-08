6c52e367c0d0505fdb85fa883fe4ae4bde732574
ranto <rantoirwanda08@gmail.com>
2024-08-08 16:04:07 +0700

    update meta data

--- src/app/agus/page.tsx
+++ src/app/agus/page.tsx
@@ -9,8 +9,21 @@ export async function generateMetadata(): Promise<Metadata> {
   // optionally access and extend (rather than replace) parent metadata
 
   return {
-    title: "Kpoin",
+    title: "kapok",
     description: "cek des",
+    openGraph: {
+        siteName: 'Kpokin',
+        title: 'Kpokin',
+        description:
+          'Hanya di Kpokin, belanjaan kamu bisa ditukar dengan hadiah istimewa dan keuntungan menarik. Dapatkan ekstra benefit dengan kumpulkan poin belanja dan ajak teman/kerbat bergabung di Kpokin.',
+        type: 'website',
+        images: {
+          type: 'image/png',
+          url: `${process.env.NEXT_PUBLIC_URL}/dragon flower.jpg`
+        },
+        url: `${process.env.NEXT_PUBLIC_URL}`,
+        locale: 'id-ID'
+      },
   };
 }
 
